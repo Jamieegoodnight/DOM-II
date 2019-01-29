547fd2141ef96de397d02b790c82144fbf294ab1
Jamie Goodnight <goodnightlambda@gmail.com>
2019-01-29 15:11:45 -0500

    Initial commit. Couple of events.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+const dontTouch=document.querySelector('.main-navigation');
+
+dontTouch.addEventListener('mouseover', e => { 
+console.log(`DON'T TOUCH THAT!`)
+})
+
+const navStuff=document.querySelectorAll(`nav a`);
+
+navStuff[0].addEventListener(`mouseover`, e => {navStuff[0].style.color=`pink`});
+
+navStuff[1].addEventListener(`mouseover`, e => {navStuff[1].style.color=`green`});
+
+navStuff[2].addEventListener(`mouseover`, e => {navStuff[2].style.color=`orange`});
